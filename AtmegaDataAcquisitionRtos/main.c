2e3e36727cbe118fe7fdd6619d475ea6d35360ae
Luca Agrippino <luca.agrippino@gmail.com>
2020-04-23 19:30:50 +0200

    Initial Commit

---
+++ AtmegaDataAcquisitionRtos/main.c
@@ -0,0 +1,18 @@
+/*
+ * AtmegaDataAcquisitionRtos.c
+ *
+ * Created: 23/04/2020 19:18:05
+ * Author : Luca
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
